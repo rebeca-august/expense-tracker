6e58441289d63f687b0aaf63fec4484004df0d0d
rebeca-august <rebeca.cazacu@gmail.com>
2020-09-18 17:58:17 +0100

    Updates

---
+++ precache-manifest.26c1541dec5d574b40ec188e93a32d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2f857e14f3e9a37ecea1c28968922be",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "33116ee2b76685094e91",
+    "url": "/expense-tracker/static/css/main.8a50243e.chunk.css"
+  },
+  {
+    "revision": "c78cdea25de16fd87f04",
+    "url": "/expense-tracker/static/js/2.57046093.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.57046093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33116ee2b76685094e91",
+    "url": "/expense-tracker/static/js/main.13d20228.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
