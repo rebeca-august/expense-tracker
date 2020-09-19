8d9fc412409a4f5c25e322c5b70feff86470c276
rebeca-august <rebeca.cazacu@gmail.com>
2020-09-19 13:09:47 +0100

    delete input lebels

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -27,24 +27,21 @@ const AddTransaction = () => {
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
         <div className="form-control">
-          <label htmlFor="text">Text</label>
           <input
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
+            placeholder="Text"
+            required
           />
         </div>
         <div className="form-control">
-          <label htmlFor="amount">
-            Amount <br />
-            (negative - expense, positive - income)
-          </label>
           <input
             type="number"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
-            placeholder="Enter amount..."
+            placeholder="Amount (negative & positive)"
+            required
           />
         </div>
         <button className="btn">Add transaction</button>
